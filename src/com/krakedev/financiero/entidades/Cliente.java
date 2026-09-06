1dddda95aad539161c1a9da361ae4d209311767b
Miizhhu1 <miizhhu@gmail.com>
2026-09-06 18:12:11 -0500

    Creamos la clase Cliente con sus respectiivos atributos y metodos

---
+++ src/com/krakedev/financiero/entidades/Cliente.java
@@ -0,0 +1,47 @@
+package com.krakedev.financiero.entidades;
+
+public class Cliente {
+	private String cedula;
+	private String nombre;
+	private String apellido;
+
+	// constructor
+	public Cliente(String cedula, String nombre, String apellido) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	// metodos
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	//metodo imprimir
+	public void imprimir() {
+		String mensaje;
+		mensaje ="Cedula: "+cedula+", Nombre: "+nombre+", Apellido: "+ apellido;
+		System.out.println(mensaje);
+	}
+
+}
